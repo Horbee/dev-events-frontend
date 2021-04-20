d936da33837f7cf526a188f6fae45018d0440ae2
Norbee <n.horox@gmail.com>
2021-04-20 18:20:47 +0200

    Cleanup home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
 import { Layout } from "@/components/Layout";
+import styles from "@/styles/Home.module.css";
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
+      <div className={styles.landing}>
+        <h1>Welcome To Dev Events</h1>
+        <p className="fs-5">Find the hottest developer events nearby!</p>
+      </div>
     </Layout>
   );
 }
