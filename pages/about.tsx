457b66f6110504808bab10413d8bbe5611c2efce
Norbert Horgas <norbert.horgas@mathema.de>
2021-04-26 12:32:07 +0200

    Add better styles to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
 
 import { Layout } from "@/components/Layout";
+import { Container, Heading, Text, VStack } from "@chakra-ui/react";
 
 export default function AboutPage() {
   return (
-    <Layout title="About Dev events">
-      <h1>About</h1>
-      <p>This is an app to find the latest Developer events.</p>
-      <p>Version: 1.0.0</p>
-      <Link href="/">Home</Link>
+    <Layout title="About Company events">
+      <Container my="5" maxW="lg">
+        <VStack align="flex-start">
+          <Heading as="h1" fontSize="4xl">
+            About
+          </Heading>
+          <Text>
+            This is an app to find the upcoming events of your Company.
+          </Text>
+          <Text>Version: 1.0.0</Text>
+          <Link href="/">
+            <Text color="blue.400" cursor="pointer">
+              Home
+            </Text>
+          </Link>
+        </VStack>
+      </Container>
     </Layout>
   );
 }
